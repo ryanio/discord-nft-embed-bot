321bf6922dfd2e11cde5471d7064643793a165f0
Ryan Ghods <ryan@ryanio.com>
2022-12-16 18:39:13 -0800

    use original quality for faster response

--- index.ts
+++ index.ts
@@ -114,7 +114,7 @@ const addrForOpenseaUsername = async (username: string, log: Log) => {
 }
 
 const imageForAsset = (asset: any) => {
-  return asset.image_url.replace(/w=(\d)*/, 'w=3000')
+  return asset.image_url
 }
 
 const sortPriceASC = (a: any, b: any) => {
